51e6cdd5f4655d4d23d4896d6de72488b139ac1e
Philippe Plaxine <philippe.plaxine02@gmail.com>
2019-08-02 16:00:00 +0200

    configuration files for CRUD MS added

--- Biocycle/productBatchCRUD/src/main/java/com/biocycle/productBatchCRUD/ProductBatchCrudApplication.java
+++ Biocycle/productBatchCRUD/src/main/java/com/biocycle/productBatchCRUD/ProductBatchCrudApplication.java
@@ -2,8 +2,10 @@ package com.biocycle.productBatchCRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductBatchCrudApplication {
 
 	public static void main(String[] args) {
--- Biocycle/staffCRUD/src/main/java/com/biocycle/staffCRUD/StaffCrudApplication.java
+++ Biocycle/staffCRUD/src/main/java/com/biocycle/staffCRUD/StaffCrudApplication.java
@@ -2,8 +2,10 @@ package com.biocycle.staffCRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StaffCrudApplication {
 
 	public static void main(String[] args) {
