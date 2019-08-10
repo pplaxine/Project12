e74cdddf8263277adcfe11e70bff390ff16480b3
Philippe Plaxine <philippe.plaxine02@gmail.com>
2019-08-10 21:51:27 +0200

    CustomerService MS block added

---
+++ Biocycle/customerManagmentService/src/main/java/com/biocycle/customerManagmentService/FunctonalitiesRelatedOrganisationsApplication.java
@@ -0,0 +1,13 @@
+package com.biocycle.customerManagmentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctonalitiesRelatedOrganisationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctonalitiesRelatedOrganisationsApplication.class, args);
+	}
+
+}
