6ec22ecf40c99d287dc1b6b8a094ab37f5a0f52e
Philippe Plaxine <philippe.plaxine02@gmail.com>
2019-08-15 06:15:06 +0200

    Ribbon removed and reimplemented, loadbalancing issue fixed

--- Biocycle/InventoryService/src/main/java/com/biocycle/InventoryService/proxy/ProductStorageMSProxy.java
+++ Biocycle/InventoryService/src/main/java/com/biocycle/InventoryService/proxy/ProductStorageMSProxy.java
@@ -1,16 +1,12 @@
 package com.biocycle.InventoryService.proxy;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.biocycle.InventoryService.bean.ProductBatchBean;
 
 @FeignClient(name = "productStorageService")
 @RibbonClient(name = "productStorageService")
