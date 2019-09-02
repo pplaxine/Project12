d0485b105591b3acee6019ad1601335492579fc3
Philippe Plaxine <philippe.plaxine02@gmail.com>
2019-09-02 22:06:11 +0200

    Staff creation functionnaly implemented

--- Biocycle/entWebApp/src/main/java/com/biocycle/entWebApp/controller/StaffManagmentController.java
+++ Biocycle/entWebApp/src/main/java/com/biocycle/entWebApp/controller/StaffManagmentController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.biocycle.entWebApp.dto.StaffBeanDto;
 import com.biocycle.entWebApp.service.StaffManagmentManager;
 
 @Controller
@@ -16,5 +18,15 @@ public class StaffManagmentController {
 	@RequestMapping("/hr/staff")
 	public String staffList(Model model) {
 		return staffManagmentManager.staffList(model);
+	}
+	
+	@RequestMapping("/hr/staff/form")
+	public String staffForm(StaffBeanDto staffBeanDto, Model model) {
+		return staffManagmentManager.staffForm(staffBeanDto, model);
+	}
+	
+	@RequestMapping("/hr/staff/form/create")
+	public String createStaff(StaffBeanDto staffBeanDto, Model model, RedirectAttributes red) {
+		return staffManagmentManager.createStaff(staffBeanDto, model,red);
 	}
 }
--- Biocycle/entWebApp/src/main/java/com/biocycle/entWebApp/service/StaffManagmentManager.java
+++ Biocycle/entWebApp/src/main/java/com/biocycle/entWebApp/service/StaffManagmentManager.java
@@ -3,10 +3,14 @@ package com.biocycle.entWebApp.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.biocycle.entWebApp.dto.OrganisationBeanDto;
 import com.biocycle.entWebApp.dto.StaffBeanDto;
 import com.biocycle.entWebApp.proxy.StaffCRUDMSProxy;
 
@@ -33,4 +37,29 @@ public class StaffManagmentManager {
 		}
 		return "staffList";
 	}
+	
+	public String staffForm(StaffBeanDto staffDtoBean, Model model) {
+		return"staffForm";
+	}
+	
+	public String createStaff(StaffBeanDto staffBeanDto, Model model, RedirectAttributes red) {
+		try {
+			staffCRUDMSProxy.addStaff(staffBeanDto);
+			String info = "New staff successfully created.";
+			red.addFlashAttribute("info", info);
+
+		} catch (ResponseStatusException rse) {
+			String error ="Could not register your entry. Please try again.";
+			model.addAttribute("error", error);
+			return "staffForm";
+		
+		} catch (Exception e) {
+			String error ="Could not register your entry. Please try again.";
+			model.addAttribute("error", error);
+			return "staffForm";
+		}
+		
+		return"redirect:/hr/staff/form";
+	}
+	
 }
